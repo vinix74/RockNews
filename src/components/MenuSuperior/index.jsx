532b29a9a5a998a83597c9cdc24c27241308eeec
Vinicius <vinix.fatos@hotmail.com>
2020-02-21 13:15:04 -0300

    subindo o projeto

---
+++ src/components/MenuSuperior/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap';
+
+import Logo from '../../assets/logo.jpg';
+
+const MenuSuperior = () => (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+            <img src={Logo} alt="logo" />
+
+            Rock News
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Nav>
+                <Nav.Link href="#deets">More deets</Nav.Link>
+                <Nav.Link eventKey={2} href="#memes">
+                    Dank memes
+      </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+)
+
+export default MenuSuperior;
+
